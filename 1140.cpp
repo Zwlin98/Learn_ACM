5e36450f027ef0cbd5220122286184d1a9ac458b
Zhanweilin <1436631269@qq.com>
2017-05-07 12:39:44 +0800

    5-7

---
+++ 1140.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int a[100][100]={0};
+	int n,i,j;
+	scanf("%d",&n);
+	for (i = 0; i < n; ++i)
+		a[i][i]=1;
+	for (i = 0; i < n; ++i)
+	{
+		for (j = 0; j < n; ++j)
+		{
+			printf("d ",a[i][j]);
+		}
+	}
+	return 0;
+}
